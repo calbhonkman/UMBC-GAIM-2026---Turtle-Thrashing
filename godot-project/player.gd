2366f1ee821d4754d6b2ed215f603b4c49313c24
Damon Gullion <damong1@umbc.edu>
2026-02-14 00:55:14 -0500

    Added camera smoothing
    The camera will now smoothly reposition itself ahead of the player in the direction they are moving, to provide a better view of the area they're headed to.

--- godot-project/player.gd
+++ godot-project/player.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
 
-@export var SPEED = 100
+@export var SPEED = 200
+@export var CAMERA_DIST = 100
+
+@onready var camera = $Camera2D
+
+
 
 func _process(delta):
 	print(position)
 	var movement_vector = Input.get_vector("left","right","up","down")
+	camera.global_position = lerp(camera.global_position, global_position + movement_vector * CAMERA_DIST, delta)
+	print(movement_vector)
 	if movement_vector.length() != 0:
 		move_and_collide(movement_vector * SPEED * delta)
