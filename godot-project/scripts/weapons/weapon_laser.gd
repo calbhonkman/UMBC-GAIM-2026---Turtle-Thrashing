b3bc1d6611d332aed3ffe795e08870bb819cc2a5
Damon Gullion <damong1@umbc.edu>
2026-03-05 19:24:22 -0500

    Laser reworked
    - Laser now extends offscreen
    - Laser now does reduced damage but increased knockback
    - Laser now has an upgrade for knockback

--- godot-project/scripts/weapons/weapon_laser.gd
+++ godot-project/scripts/weapons/weapon_laser.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-@export var DAMAGE: float = 4.0
+@export var DAMAGE: float = 0.5
 @export var COOLDOWN: float = 10.0
 @export var BULLET: Resource
-@export var LIFETIME: float = 2.0
-@export var KNOCKBACK: float = 100.0
+@export var LIFETIME: float = 5.0
+@export var KNOCKBACK: float = 200.0
 
 @onready var player = $".."
 
@@ -15,7 +15,6 @@ var size_mod = 1.0
 
 var bullet = null
 var b_cooldown = 0.0
-var b_target = null
 var b_lifetime = 0.0
 
 func _ready():
@@ -34,14 +33,9 @@ func _process(delta):
 		if b_lifetime <= 0.0:
 			bullet.queue_free()
 		
-		if b_target:
-			bullet.global_position = global_position + (b_target.global_position - global_position)/2
-			bullet.rotation = (b_target.global_position - global_position).angle()
-			bullet.scale.x = (b_target.global_position - global_position).length() / 40
-			bullet.scale.y = size_mod
-			bullet.visible = true
-		else:
-			bullet.queue_free()
+		bullet.rotation = (get_global_mouse_position() - global_position).angle()
+		bullet.scale.y = size_mod
+		bullet.visible = true
 		
 		for area in bullet.get_overlapping_areas():
 			if area.has_meta("enemy"):
@@ -50,22 +44,11 @@ func _process(delta):
 				area.global_position += (knockback_dir / knockback_dir.length()) * KNOCKBACK * delta
 
 	
-	elif b_cooldown <= 0.0:
-		b_target = find_target()
-		if b_target:
-			bullet = BULLET.instantiate()
-			add_child(bullet)
-			bullet.global_position = global_position + (b_target.global_position - global_position)/2
-			bullet.rotation = (b_target.global_position - global_position).angle()
-			b_lifetime = LIFETIME
-			b_cooldown = COOLDOWN
-
-func find_target():
-	var target_enemy = null
-	for area in get_overlapping_areas():
-		if area.has_meta("enemy") and (target_enemy == null or (area.global_position-global_position).length() > (target_enemy.global_position-global_position).length()):
-			target_enemy = area
-	return target_enemy
+	elif b_cooldown <= 0.0 and bullet == null:
+		bullet = BULLET.instantiate()
+		add_child(bullet)
+		b_lifetime = LIFETIME
+		b_cooldown = COOLDOWN
 
 func get_upgrade():
 	if unlocked:
@@ -78,4 +61,6 @@ func upgrade(index: int):
 			unlocked = true
 			visible = true
 		1:
-			size_mod *= 1.5
+			size_mod += 1.0
+		2:
+			KNOCKBACK *= 1.25
