8d1bb602e6e7b28e84e3ae2de1630f3ce742fabe
honkman <diamondarmyl.64@gmail.com>
2026-04-06 15:22:57 -0400

    Started work on seagull (Unfinished)

---
+++ godot-project/prefabs/enemies/(enemy)_seagull.gd
@@ -0,0 +1,75 @@
+extends Area2D
+
+@onready var player = $"/root/Node2D/Player"
+@onready var sprite = $AnimatedSprite2D
+@onready var warning_sprite = $WarningSprite
+@onready var dive_box = $DiveBox
+@onready var hitbox = $CollisionShape2D
+const EXP = preload("uid://bln5qlwy18sjf")
+
+@export var MAX_HEALTH: float = 1.0
+var health: float = MAX_HEALTH
+
+@export var BASE_MOVE_SPEED: float = 40.0
+var move_speed: float = BASE_MOVE_SPEED
+
+var stunned: bool = false
+@export var STUN_RESIST: float = 0.0 # Percent
+var stun_timer: float = 0.0 # Seconds
+
+var dying: bool = false
+@export var DYING_TIME: float = 0.5
+var dying_timer: float = DYING_TIME
+@export var EXP_AMOUNT: int = 1
+
+# Other enemies may not have these variables:
+var mode = "default"
+var mode_timer
+@export var STRUGGLE_TIME: float = 5.0
+@export var DIVE_SPEED: float = 1000.0
+
+func _ready():
+	hitbox.disabled = true
+
+func _process(delta):
+	scale = Vector2(1,1) * (0.75 + clamp(0.25 * health / MAX_HEALTH, 0.0, 0.25))
+	if dying and dying_timer > 0.0:
+		monitorable = false
+		monitoring = false
+		dying_timer -= delta
+		scale = Vector2(1,1) * clamp(0.75 * dying_timer / DYING_TIME, 0.0, 0.75)
+		sprite.modulate = Color(1,0,0,clamp(0.5 * dying_timer / DYING_TIME, 0.0, 0.5))
+	elif dying and dying_timer <= 0.0:
+		AudioManager.poof.play()
+		for i in EXP_AMOUNT:
+			var new_xp = EXP.instantiate()
+			get_parent().add_child(new_xp)
+			new_xp.global_position = global_position
+		queue_free()
+	elif player:
+		if stunned:
+			stun_timer -= delta
+			if stun_timer <= 0:
+				stunned = false
+				sprite.modulate = Color(1,1,1,1)
+		else: 	
+			var playerDirection = player.global_position - global_position
+			playerDirection = playerDirection / playerDirection.length()
+			global_position += playerDirection * delta * move_speed
+			sprite.scale.x = -1 * abs(sprite.scale.x) * playerDirection.x / abs(playerDirection.x) if playerDirection.x != 0 else sprite.scale.x
+			sprite.play("walk")
+
+func scale_health(s: float):
+	MAX_HEALTH *= s
+	health = MAX_HEALTH
+
+func damage(dmg: float):
+	health -= dmg
+	if health <= 0.0:
+		dying = true
+		
+func stun(time: float):
+	if stunned != true:
+		stunned = true
+		stun_timer = time
+		sprite.modulate = Color(0.788, 0.788, 0.0, 1.0)
